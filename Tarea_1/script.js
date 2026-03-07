681384b40977445c2e10f4475c1108121bebe62f
Marilyn Daniela Sobalvarro Lopez <marilyn24ix@gmail.com>
2026-03-07 15:18:17 -0600

    Tarea_01

---
+++ Tarea_1/script.js
@@ -0,0 +1,14 @@
+// EJERCICIO 1 /
+
+let number = 24
+let result = (number >= 10 && number <= 20) || (number  >= 40 && number <= 60)
+
+console.log("El calculo de rangos para el numero " + number + " es: " + result)
+
+// EJERCICIO 2 / 
+
+let dogAge = 7
+let humanAge = 2 * 10 + (dogAge - 2) * 4
+
+
+console.log("El perrito de " + dogAge+ " años tendría " + humanAge + " si fuera humano" )
