2df89e6c5840424218ee8a2192b4e06ef390824e
Marilyn Daniela Sobalvarro Lopez <marilyn.sobalvarro@CETAV-B6-10.local>
2026-03-18 16:58:47 -0600

    Clase 8

---
+++ Clase-8/main.js
@@ -0,0 +1,97 @@
+/* 
+LOOP
+(Se repite en bucle si la condicion es cierta)
+/Cuando la condicion no es cierta no muestra nada
+*/ 
+
+// < menor a | > mayor a | <= menor o igual a | >= mayor o igual a | == igual a | != diferente a 
+//while (5 < 2) {
+//    console.log('EXECUTE');
+//}
+
+//let index = 0;
+//while (index < 10) {
+//    console.log('EXECUTE');
+//   index = 15;
+//}
+
+/*
+let cantidadDeEstudiantes = 20;
+
+while (cantidadDeEstudiantes > 0) {
+    console.log('Enviar correo a estudiante', cantidadDeEstudiantes);
+    cantidadDeEstudiantes = cantidadDeEstudiantes -1;
+}
+*/
+
+/*   % RESIDUO de una division   */
+/*Residuo 0 es par | Residuo 1 es impar*/
+
+/*let cantidadQueFalta = 20;
+
+while (cantidadQueFalta > 0) {
+    if (cantidadQueFalta % 2 === 0) {
+        console.log('SORPRESA para estudiante', cantidadQueFalta);
+    } else {
+    console.log('Enviar correo regular a estudiante', cantidadQueFalta);
+    }
+    cantidadQueFalta = cantidadQueFalta -1;
+}*/
+
+/*
+let iteraciones = 100
+
+while (iteraciones > 0) {
+    if (iteraciones % 3 === 0) {
+        console.log ('El numero', iteraciones, 'es multiplo de 3');
+    } else if (iteraciones % 5 === 0) {
+        console.log ('El numero', iteraciones, 'es multiplo de 5');
+    } else if (iteraciones % 3 === 0 && iteraciones % 5 === 0) {
+        console.log ('El numero', iteraciones, 'es multiplo de 3 y 5');
+    }
+    iteraciones = iteraciones -1;
+}
+*/
+
+/*
+let iteracion = 100;
+
+while (iteracion > 0) {
+    if (iteracion % 3 === 0 && iteracion % 5 === 0) {
+        console.log( 'El numero', iteracion, 'es multiplo de 3 y 5');
+    } else if (iteracion % 3 === 0) {
+        console.log('El numero', iteracion, 'es multiplo de 3');
+    } else if (iteracion % 5 === 0) {
+        console.log('El numero', iteracion, 'es multiplo de 5');
+    }
+    iteracion = iteracion -1;
+}
+    */
+
+/*
+let iteracion = 100;
+
+while (iteracion > 0) {
+    const residuoDe5 = iteracion % 5 === 0;
+    const residuoDe3 = iteracion % 3 === 0;
+    if (residuoDe5 && residuoDe3) {
+        console.log( 'El numero', iteracion, 'es multiplo de 3 y 5');
+    } else if (iteracion % 3 === 0) {
+        console.log('El numero', iteracion, 'es multiplo de 3');
+    } else if (iteracion % 5 === 0) {
+        console.log('El numero', iteracion, 'es multiplo de 5');
+    } else {
+        console.log('El numero', iteracion, 'no es multiplo de 3 ni de 5');
+    }
+    iteracion = iteracion -1;
+}
+*/
+
+// INPUT
+let number = 3;
+
+// OUTPUT
+while () {
+    if ()
+    console.log()
+}
