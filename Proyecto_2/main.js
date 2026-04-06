c207d395511ddcd2cab3aab346ee6b8b3dd66ba8
Marilyn Daniela Sobalvarro Lopez <marilyn24ix@gmail.com>
2026-04-06 00:43:53 -0600

    proyecto 2

---
+++ Proyecto_2/main.js
@@ -0,0 +1,31 @@
+// Proyecto 2 - Cajero
+
+let cantidadDeClientes = 25;
+let totalSegundos = 0;
+
+for (let i = 1; i <=cantidadDeClientes; i++) {
+    if(i % 2 === 0) {
+        totalSegundos += (i * 60) + 45;
+    } else { 
+        totalSegundos += 20;
+    }
+
+    if (i % 10 === 0) {
+    totalSegundos += (18 * 60 ) + 11
+    } else if (i % 5 ===  0) {
+        totalSegundos += (37 * 60)
+    }
+}
+
+
+let horas = Math.floor(totalSegundos / 3600)
+let minutos = Math.floor((totalSegundos % 3600) / 60)
+let segundos = totalSegundos % 60;
+
+if (horas > 0) {
+    console.log('A el cajero le va a tomar ' + horas + ' horas ' + minutos + ' minutos y ' + segundos + ' segundos ')
+} else if (minutos > 0) { 
+    console.log('A el cajero le va a tomar ' + minutos + ' minutos y ' + segundos + ' segundos ')
+} else {
+    console.log('A el cajero le va a tomar ' + segundos + ' segundos ')
+} 
